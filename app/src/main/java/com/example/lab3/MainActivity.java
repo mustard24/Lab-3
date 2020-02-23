b516250e59cdceb36b99e3771738f285239d7a84
Dustin Moody <dmoody3@stu.jsu.edu>
2020-02-23 17:49:22 -0600

    Made most functionality work on my calculator

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -11,8 +11,21 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity {
+    Boolean rightFlag = false;
+    String operator = "";
+    String operatorStorage= "";
+    Boolean positive = true;
+    String leftSide;
+    String rightSide = "";
+    Boolean textDelete = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +34,126 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+
+
+    }
+    public void plusMinusPressed(View view) {
+        TextView numBox = (TextView) findViewById(R.id.NumBox);
+        BigDecimal check = new BigDecimal(numBox.getText().toString());
+        numBox.setText(check.negate().toString());
+    }
+    public void numPressed(View view){
+        int id = view.getId();
+        TextView numBox = (TextView) findViewById(R.id.NumBox);
+        Button buttonPressed = (Button) findViewById(id);
+        if(textDelete){
+            leftSide = numBox.getText().toString();
+            numBox.setText(buttonPressed.getText().toString());
+            rightFlag = true;
+        }
+        else{
+            numBox.setText(numBox.getText() + buttonPressed.getText().toString());
+        }
+        textDelete = false;
+    }
+    public void calculationPressed(View view) {
+        TextView numBox = (TextView) findViewById(R.id.NumBox);
+        if (!numBox.getText().toString().equals("")){
+            System.out.println("Made it in here!");
+            int id = view.getId();
+            Button buttonPressed = (Button) findViewById(id);
+            operator = buttonPressed.getText().toString();
+
+            if (rightFlag == false){
+                operatorStorage = operator;
+                leftSide = numBox.getText().toString();
+                numBox.setText("");
+                rightFlag = true;
+
             }
-        });
+            else{
+
+                    rightSide = numBox.getText().toString();
+                    calculate();
+
+            }
+        }
+
+
+
+    }
+    public void clear(View view){
+        TextView numBox = (TextView) findViewById(R.id.NumBox);
+        numBox.setText("");
+        leftSide = "";
+        rightSide= "";
+        rightFlag = false;
+
+    }
+    public void squareRoot(View view){
+        TextView numBox = (TextView) findViewById(R.id.NumBox);
+        double sqrdNumber = Double.valueOf(numBox.getText().toString());
+        sqrdNumber = Math.sqrt(sqrdNumber);
+        numBox.setText(String.valueOf(sqrdNumber));
+    }
+    public void calculate(){
+        if(leftSide ==""){
+            return;
+        }
+        BigDecimal leftNumber = new BigDecimal(leftSide);
+        BigDecimal rightNumber = new BigDecimal(rightSide);
+        BigDecimal answer = new BigDecimal(0);
+        TextView numBox = (TextView) findViewById(R.id.NumBox);
+        if (operator.equals("=")){
+            switch(operatorStorage) {
+                case "\u00D7":  //Multiplication
+                    answer = leftNumber.multiply(rightNumber);
+                    break;
+                case "\u00F7":  //Division
+                    answer = leftNumber.divide(rightNumber);
+                    break;
+                case "+":
+                    answer = leftNumber.add(rightNumber);
+                    break;
+                case "-":
+                    answer = leftNumber.subtract(rightNumber);
+                    break;
+                case "%":
+                    answer = leftNumber.remainder(rightNumber);
+                    break;
+                default:
+
+            }
+        }
+        else{
+            switch(operator) {
+                case "\u00D7":  //Multiplication
+                    answer = leftNumber.multiply(rightNumber);
+                    break;
+                case "\u00F7":  //Division
+                    answer = leftNumber.divide(rightNumber);
+                    break;
+                case "+":
+                    answer = leftNumber.add(rightNumber);
+                    break;
+                case "-":
+                    answer = leftNumber.subtract(rightNumber);
+                    break;
+                case "%":
+                    answer = leftNumber.remainder(rightNumber);
+                    break;
+                default:
+
+        }
+            textDelete = true;
+
+        }
+        numBox.setText(answer.toString());
+        leftSide = answer.toString();
+        rightSide = "";
+        rightFlag = false;
     }
 
     @Override
